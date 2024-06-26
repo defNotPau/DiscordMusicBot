ee696d0c98bf41b8c7ea47bb0013699f0a1f542a
manpau_23 <pablo.cabarcas@gmail.com>
2024-06-25 22:28:19 -0400

    nothing works yet :)
    eeeeeee i had to make some new stuff :) (like an api that i can freely modify without being yelled at)

--- api/pau_main.py
+++ api/pau_main.py
@@ -6,6 +6,7 @@ import os
 from flask import Response,Flask, stream_with_context
 from mutagen.mp3 import MP3
 import flask
+import json
 
 app = Flask(__name__)
 
@@ -29,6 +30,13 @@ def download(url,track_name):
     video.close()
     os.remove(track_name+".mp4")
 
+    seconds = link.length
+
+    minutes = seconds // 60
+    remaining_seconds = seconds % 60
+
+    return (link.title, f"{minutes:02}:{remaining_seconds:02}", link.author)
+
 @app.route("/download/<track_name>")
 def main_download(track_name):
     main_query = "https://www.youtube.com/results?search_query="+yformat(track_name)
@@ -37,9 +45,10 @@ def main_download(track_name):
     video_id = re.findall(r"watch\?v=(\S{11})", query_get.text)
 
     video_link = "https://youtube.com/watch?v="+video_id[0]
-    download(video_link,track_name)
-
-    return flask.send_file("../"+track_name+".mp3", mimetype="audio/mpeg")
+    video_info = download(video_link,track_name)
+    jsondic = {"name": video_info[0],"author":video_info[2]}
+    
+    return (json.dumps(jsondic))
 
 @app.route("/download/<track_name>/<artist_name>")
 def full_download(track_name, artist_name):
@@ -49,6 +58,10 @@ def full_download(track_name, artist_name):
     video_id = re.findall(r"watch\?v=(\S{11})", query_get.text)
 
     video_link = "https://youtube.com/watch?v="+video_id[0]
-    download(video_link,track_name)
+    video_name = download(video_link,track_name)
 
     return flask.send_file("../"+track_name+".mp3", mimetype="audio/mpeg")
+
+if __name__ == '__main__':
+   app.run(debug=True)
+
