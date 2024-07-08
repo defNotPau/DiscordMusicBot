224be1031a48464d32953a4326f19c14abb14e5d
whiter0s <randomosintdude@protonmail.com>
2024-07-07 22:23:10 -0500

    Fixed much stuff

--- api/main.py
+++ api/main.py
@@ -1,24 +1,24 @@
-#import necessary comments
 import requests
-import re
+import regex as re
 from moviepy.editor import *
 from pytube import YouTube
 import os
-from flask import Response,Flask
-from mutagen.mp3 import MP3
+from flask import Flask
+import flask
+import json
 
 
 
 app = Flask(__name__)
 
 #function to replace spaces with + for youtube query
-def replace_youtube(string):
+def ytformat(string):
     #make a main string splitted in between whitespaces
     main = string.split()
     #replace the whitespace with a + for youtube
     main_join = "+".join(main)
     return main_join
-
+    
 #function for downloading file
 def download(url,track_name):
     link = YouTube(url)
@@ -31,37 +31,36 @@ def download(url,track_name):
     audio = video.audio
     #write the mp3
     audio.write_audiofile(track_name+".mp3")
+    audio.close()
+    video.close()
     os.remove(track_name+".mp4")
-    
+    print(link.author)
 #the 4o4 page
 @app.route("/")
 def four():
     return("Ooops, this page doesnt exist go to /help to see existing pages")
-
 #main app route
-@app.route("/download/<artist_name>/<track_name>")
-def main(track_name,artist_name):
-    #artist name
-    artist = artist_name
-    #track name
-    track_name = track_name
-    #formulate the main search query for youtube
-    main_query = "https://www.youtube.com/results?search_query="+replace_youtube(artist)+"-"+replace_youtube(track_name)
-    query_get = requests.get(main_query)
-    
-    #search for video id in response
-    video_id = re.findall(r"watch\?v=(\S{11})", query_get.text)
-    #make a youtube video link
-    video_link = "https://youtube.com/watch?v="+video_id[0]
-    #use try so you can clean up the mp3
-    download(video_link,track_name)
+@app.route("/download/<result>/<artist_name>/<track_name>")
+def main_download(track_name,artist_name,result):
     try:
-        #return file
-        return Response(open(track_name+".mp3", "rb"), mimetype="audio/mpeg")
+        #artist name
+        artist = artist_name
+        #track name
+        track_name = track_name
+        #formulate the main search query for youtube
+        main_query = "https://www.youtube.com/results?search_query="+ytformat(artist)+"-"+ytformat(track_name)
+        query_get = requests.get(main_query)
+        
+        #search for video id in response
+        video_id = re.search(r"watch\?v=(\S{11})", query_get.text)
+        #make a youtube video link
+        video_link = "https://youtube.com/watch?v="+video_id[int(result)]
+        download(video_link,track_name)
+        return flask.send_file("../"+track_name+".mp3", mimetype="audio/mpeg")
+    except KeyError:
+        return str(-1)
 
-    finally:
-        #clean up mp3
-        os.remove(track_name+".mp3")
+    
 
 #add help route
 @app.route("/help")
@@ -69,44 +68,38 @@ def help():
     #return help message
     return "To get the mp3 file you go, serverip:5000/artist/track_name and to add spaces just use a +\n Also, to get info about a song go, serverip:5000/artist/track_name and you will get duration, artist, and video title"  
 
-@app.route("/info/<artist>/<track_name>") 
-def info(track_name,artist):
-    main_query = "https://www.youtube.com/results?search_query="+replace_youtube(artist)+"-"+replace_youtube(track_name)
-    query_get = requests.get(main_query)
-    #search for video id in response
+@app.route("/info/<result>/<artist>/<track_name>") 
+def info(track_name,artist,result):
+    #Process The link so its useable for pytube
+    query_get = requests.get("https://www.youtube.com/results?search_query="+ytformat(artist)+"-"+ytformat(track_name))
+    #Now transform the video list into ussable links
     video_id = re.findall(r"watch\?v=(\S{11})", query_get.text)
-    #make a youtube video link
-    video_link = "https://youtube.com/watch?v="+video_id[0]
-    #donwload the video to get the duration
-    download(video_link,track_name)
-    #get all the info
-    artist_name = artist
-    track_name_main = track_name
-    if artist.find("+") !=-1:
-        artist_replace = artist.replace("+", " ")
-        artist_split = artist_replace.split()
-        artist_name = " ".join(artist_split)
-    elif artist.find("+") ==-1:
-        pass
-    if track_name.find("+") !=-1:
-        track_name_replace = track_name.replace("+", " ")
-        track_name_split = track_name_replace.split()
-        track_name_main = " ".join(track_name_split)
-    elif track_name.find("+") == -1:
-        pass
-    video_name = re.findall(track_name_main, query_get.text)
-    video_artist = re.findall(artist_name, query_get.text)
-    #get the duration of the mp3
-    audio = MP3("../api/output"+track_name+".mp3")
-    audio_info = audio.info
-    lenght = int(audio_info.length)
-    mins = lenght // 60
-    secs  = str(lenght)
-    #remove the mp3
-    os.remove(track_name+".mp3")
-    #return the data
-    return str(video_artist[0])+"-"+str(video_name[0])+" "+str(mins)+":"+secs[:-1]
+    video_link = "https://youtube.com/watch?v="+video_id[int(result)]
+    
+    #link for researching info
+    link = YouTube(video_link)
 
+    #Exception handling
+    try:
+        artist_name = link.author
+        track = link.title
+    except:
+        return("Could not find song")
+    print(artist_name)
+    #get the duration of the mp3
+    secs = str(link.length)
+    mins = int(secs) // 60
+    data = {
+        "artist": artist_name,
+        "name": track,
+        "lenght": str(mins)+":"+secs[:-1],
+        "id": track_name
+    }
+    #return the data
+    return json.dumps(data)
+@app.route("/del/<id>")
+def delete(id):
+    os.remove(id)
 #start the app
 if __name__ == '__main__':
-   app.run()
+   app.run(debug=True)
