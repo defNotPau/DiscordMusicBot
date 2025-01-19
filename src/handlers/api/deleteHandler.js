86318950a93dbb8b78d209ac2acf02836bb14fc5
defNotPau <pablo.cabarcas@gmail.com>
2025-01-19 11:41:16 -0500

    apiHandlers seem good

--- src/handlers/api/deleteHandler.js
+++ src/handlers/api/deleteHandler.js
@@ -1,6 +1,7 @@
-const apiUrl = "http://127.0.0.1:5000/del";
+const apiUrl = new URL("http://127.0.0.1:5000/del");
 
 module.exports = async () => {
-    const serverResult = await fetch(url);
+
+    const serverResult = await fetch(apiUrl.href);
     return (serverResult);
 }
